18a75b186a881949e2d5b80f9bd6eaaae96fa033
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-08 16:57:26 +0100

    Add project files.

---
+++ matematica/FunzioniMatematica.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace matematica
+{
+    class FunzioniMatematica
+    {
+        public static int Somma(int a, int b)
+        {
+            int somma = a + b;
+            return somma;
+        }
+
+        public static double SommaDouble(double a, double b)
+        {
+            double somma = a + b;
+            return somma;
+        }
+        public static double Sottrazione(double a, double b)
+        {
+            double sottrazione = a - b;
+            return sottrazione;
+        }
+        public static double Moltiplicazione(double a, double b)
+        {
+            double moltiplicazione = a * b;
+            return moltiplicazione;
+        }
+        public static double Divisione(double a, double b)
+        {
+            double divisione = a / b;
+            return divisione;
+        }
+        public static long Potenza(int a, int b)
+        {
+            int i;
+            long potenza = 1;
+            for (i = 0; i < b; i++)
+            {
+                potenza = potenza * a;
+            }
+            return potenza;
+        }
+        public static long CalcolaFattoriale(int a)
+        {
+            long fattoriale = 1;
+            for (int i = 2; i <= a; i++)
+            {
+                fattoriale = fattoriale * i;
+            }
+            return fattoriale;
+        }
+        public static int MinorTra(int n1, int n2, int n3)
+        {
+            int Min = Math.Min(n1, Math.Min(n2, n3));
+            return Min;
+        }
+        public static int MaggioreTra(int n1, int n2, int n3)
+        {
+            int max = Math.Max(n1, Math.Max(n2, n3));
+            return max;
+        }
+        public static double inverso(double a)
+        {
+            double inverso = a * -1;
+            return inverso;
+        }
+        public static bool isPari(int a)
+        {
+            return a % 2 == 0;
+        }
+        public static bool isdispari(int a)
+        {
+            return a % 2 == 1;
+        }
+        public static bool isPositivo(int a)
+        {
+            return a > 0;
+        }
+        public static bool isNegativo(int a)
+        {
+            return a < 0;
+        }
+        public static bool isIntero(double a)
+        {
+            return a==(int)a;
+        }
+
+    }
+}
+
---
+++ matematica/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace matematica
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int somma = FunzioniMatematica.Somma(3, 4);
+            Console.WriteLine($"la somma intera è: {somma}");
+            double sommaDouble = FunzioniMatematica.SommaDouble(3.0,4.0);
+            Console.WriteLine($"la somma decimale è: {sommaDouble}");
+            double moltiplicazione = FunzioniMatematica.Moltiplicazione(3,4);
+            Console.WriteLine($"la moltiplicazione è: {moltiplicazione}");
+            double divisione = FunzioniMatematica.Divisione(3, 4);
+            Console.WriteLine($"la divisione è: {divisione}");
+            long potenza = FunzioniMatematica.Potenza(3, 4);
+            Console.WriteLine($"la potenza è: {potenza}");
+            long molt = FunzioniMatematica.CalcolaFattoriale(8);
+            Console.WriteLine($"il fattoriale è {molt}");
+            int Min = FunzioniMatematica.MinorTra(3, 4, 5);
+            Console.WriteLine($"il minimo è {Min}");
+            int max = FunzioniMatematica.MaggioreTra(3, 4, 5);
+            Console.WriteLine($"il massimo è {max}");
+            double inverso = FunzioniMatematica.inverso(3);
+            Console.WriteLine($"il numero inverso è {inverso}");
+            bool pari = FunzioniMatematica.isPari(4);
+            Console.WriteLine($"il numero è pari {pari}");
+            bool dispari = FunzioniMatematica.isdispari(4);
+            Console.WriteLine($"il numero è dispari{dispari}");
+            bool positivo= FunzioniMatematica.isPositivo(4);
+            Console.WriteLine($"il numero è positivo: {positivo}");
+            bool negativo = FunzioniMatematica.isNegativo(4);
+            Console.WriteLine($"il numero è positivo: {negativo}");
+            bool intero = FunzioniMatematica.isIntero(4.4);
+            Console.WriteLine($"il numero è intero: {intero}");
+
+        }
+    }
+}
